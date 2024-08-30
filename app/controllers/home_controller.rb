8cbc6c4826928fd88369482d341bd466bc3def7b
Adil Lari <adillari@pm.me>
2024-08-29 20:55:49 -0500

    rails g controller Home index

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+  allow_unauthenticated_access
+
+  def index
+    redirect_to boards_path if resume_session
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   get "manifest", to: "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#index"
 end
