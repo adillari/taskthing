47434c74aabe902bd457407e6c8b34e4940ed27a
Adil Lari <adillari@pm.me>
2024-08-29 21:00:16 -0500

    lint

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -12,52 +12,53 @@ module Authentication
     end
   end
 
-  private
-    def authenticated?
-      Current.session.present?
+private
+
+  def authenticated?
+    Current.session.present?
+  end
+
+  def require_authentication
+    resume_session || request_authentication
+  end
+
+
+  def resume_session
+    if session = find_session_by_cookie
+      set_current_session session
     end
+  end
 
-    def require_authentication
-      resume_session || request_authentication
+  def find_session_by_cookie
+    if token = cookies.signed[:session_token]
+      Session.find_signed(token)
     end
+  end
 
 
-    def resume_session
-      if session = find_session_by_cookie
-        set_current_session session
-      end
+  def request_authentication
+    session[:return_to_after_authenticating] = request.url
+    redirect_to new_session_url
+  end
+
+  def after_authentication_url
+    session.delete(:return_to_after_authenticating) || root_url
+  end
+
+
+  def start_new_session_for(user)
+    user.sessions.create!(user_agent: request.user_agent, ip_address: request.remote_ip).tap do |session|
+      set_current_session session
     end
+  end
 
-    def find_session_by_cookie
-      if token = cookies.signed[:session_token]
-        Session.find_signed(token)
-      end
-    end
+  def set_current_session(session)
+    Current.session = session
+    cookies.signed.permanent[:session_token] = { value: session.signed_id, httponly: true, same_site: :lax }
+  end
 
-
-    def request_authentication
-      session[:return_to_after_authenticating] = request.url
-      redirect_to new_session_url
-    end
-
-    def after_authentication_url
-      session.delete(:return_to_after_authenticating) || root_url
-    end
-
-
-    def start_new_session_for(user)
-      user.sessions.create!(user_agent: request.user_agent, ip_address: request.remote_ip).tap do |session|
-        set_current_session session
-      end
-    end
-
-    def set_current_session(session)
-      Current.session = session
-      cookies.signed.permanent[:session_token] = { value: session.signed_id, httponly: true, same_site: :lax }
-    end
-
-    def terminate_session
-      Current.session.destroy
-      cookies.delete(:session_token)
-    end
+  def terminate_session
+    Current.session.destroy
+    cookies.delete(:session_token)
+  end
 end
