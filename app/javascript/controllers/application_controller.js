6750daa74adfebc2bbb7fd7be6306aae9768f458
Adil Lari <adillari@pm.me>
2024-09-02 18:04:35 -0500

    remove old way of showing spinner

--- app/javascript/controllers/application_controller.js
+++ app/javascript/controllers/application_controller.js
@@ -3,11 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["modal"]
 
-  showSpinner() {
-    document.getElementById("modal").innerHTML =
-      '<div class="lds-ring flex items-center"><div></div><div></div><div></div><div></div></div>'
-  }
-
   removeModal() {
     this.modalTarget.remove()
   }
