96cd267a5e2f043b2b6db3c2f027e868d2dc5310
Adil Lari <adillari@pm.me>
2024-09-02 18:04:53 -0500

    use turbo events to decide when to show spinner

---
+++ app/javascript/controllers/loading_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["indicator"]
+
+  connect() {
+    document.addEventListener('turbo:before-fetch-request', this.#showSpinner.bind(this))
+    document.addEventListener('turbo:before-fetch-response', this.#hideSpinner.bind(this))
+    document.addEventListener("turbo:before-cache", this.#hideSpinner.bind(this))
+  }
+
+  disconnect() {
+    document.removeEventListener('turbo:before-fetch-request', this.#showSpinner.bind(this))
+    document.removeEventListener('turbo:before-fetch-response', this.#hideSpinner.bind(this))
+    document.removeEventListener("turbo:before-cache", this.#hideSpinner.bind(this))
+  }
+
+  #showSpinner(event) {
+    if (this.#isPrefetchRequest(event)) return
+    this.indicatorTarget.hidden = false
+  }
+
+  #hideSpinner() {
+    this.indicatorTarget.hidden = true
+  }
+
+  #isPrefetchRequest(event) {
+    return (event.detail.fetchOptions.headers["X-Sec-Purpose"] === "prefetch")
+  }
+}
