886730d37c977e290090ce035fc39618e6cc9239
Adil Lari <adillari@pm.me>
2024-09-02 18:11:06 -0500

    boards_controller delete_confirmation method

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,17 +3,47 @@ class BoardsController < ApplicationController
     @boards = Current.user.boards
   end
 
+  def show
+    @board = find_board
+  end
+
   def new
     @board = Board.new
   end
 
   def create
-    debugger
+    board = Current.user.boards.new(board_params)
+
+    if board.save
+      # TODO: turbo stream shiz
+      redirect_to boards_path
+    else
+      @errors = board.errors
+      render :new
+    end
   end
 
   def update
   end
 
+  def delete_confirmation
+    @board = find_board
+  end
+
   def destroy
+    board = find_board
+    board.destroy!
+    # TODO: turbo stream shiz
+    redirect_to boards_path
+  end
+
+private
+
+  def find_board
+    Current.user.boards.find(params[:id])
+  end
+
+  def board_params
+    params.require(:board).permit(:title)
   end
 end
