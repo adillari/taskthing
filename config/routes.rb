c5eb50ed2ccc5465e94ec4ab8ef3ef486c472431
Adil Lari <adillari@pm.me>
2024-09-02 18:01:12 -0500

    fix navigating back to signout behavior

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
-  resources "boards"
-  resource "session", only: %i[new create destroy]
+  resources "boards" do
+    get "delete_confirmation", on: :member
+  end
+  resource "session", only: %i[show new create destroy]
   resources "users", only: %i[new create destroy]
   resources "passwords", param: :token
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "home#index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up", to: "rails/health#show", as: :rails_health_check
-
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker", to: "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest", to: "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  root "home#index"
 end
