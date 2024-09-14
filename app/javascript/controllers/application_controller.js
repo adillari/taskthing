aab188a5c9ef13f053f32fb8d67013b67a2b80c1
Adil Lari <adillari@pm.me>
2024-09-14 16:28:34 -0500

    remove modal on form submit

--- app/javascript/controllers/application_controller.js
+++ app/javascript/controllers/application_controller.js
@@ -3,8 +3,16 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["modal"]
 
+  connect() {
+    document.addEventListener('turbo:submit-start', this.removeModal.bind(this))
+  }
+
+  disconnect() {
+    document.removeEventListener('turbo:submit-start', this.removeModal.bind(this))
+  }
+
   removeModal() {
-    this.modalTarget.remove()
+    this.hasModalTarget && this.modalTarget.remove()
   }
 
   stopPropagation(event) {
