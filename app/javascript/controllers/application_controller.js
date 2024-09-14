3a0732f3b9e5a449bf33688335929a6b6e559fb8
Adil Lari <adillari@pm.me>
2024-09-14 18:14:46 -0500

    consolidate stimulus loading_controller into application_controller

--- app/javascript/controllers/application_controller.js
+++ app/javascript/controllers/application_controller.js
@@ -1,14 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["modal"]
+  static targets = ["modal", "indicator"]
 
   connect() {
     document.addEventListener('turbo:submit-start', this.removeModal.bind(this))
+    document.addEventListener('turbo:before-fetch-request', this.#showSpinner.bind(this))
+    document.addEventListener('turbo:before-fetch-response', this.#hideSpinner.bind(this))
   }
 
   disconnect() {
     document.removeEventListener('turbo:submit-start', this.removeModal.bind(this))
+    document.removeEventListener('turbo:before-fetch-request', this.#showSpinner.bind(this))
+    document.removeEventListener('turbo:before-fetch-response', this.#hideSpinner.bind(this))
   }
 
   removeModal() {
@@ -17,5 +21,20 @@ export default class extends Controller {
 
   stopPropagation(event) {
     event.stopPropagation()
+  }
+
+  // private
+
+  #showSpinner(event) {
+    if (this.#isPrefetchRequest(event)) return
+    this.indicatorTarget.hidden = false
+  }
+
+  #hideSpinner() {
+    this.indicatorTarget.hidden = true
+  }
+
+  #isPrefetchRequest(event) {
+    return (event.detail.fetchOptions.headers["X-Sec-Purpose"] === "prefetch")
   }
 }
