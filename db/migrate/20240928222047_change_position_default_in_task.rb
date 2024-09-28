33c9648277690fd86f350e055a7f049aa593e2dd
Adil Lari <adillari@pm.me>
2024-09-28 17:56:13 -0500

    use default position value of 0 for new tasks

---
+++ db/migrate/20240928222047_change_position_default_in_task.rb
@@ -0,0 +1,5 @@
+class ChangePositionDefaultInTask < ActiveRecord::Migration[8.0]
+  def change
+    change_column_default :tasks, :position, 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_09_23_045416) do
+ActiveRecord::Schema[8.0].define(version: 2024_09_28_222047) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -46,7 +46,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_09_23_045416) do
     t.datetime "updated_at", null: false
     t.string "title", null: false
     t.text "description"
-    t.integer "position", null: false
+    t.integer "position", default: 0, null: false
     t.index ["lane_id"], name: "index_tasks_on_lane_id"
   end
 
