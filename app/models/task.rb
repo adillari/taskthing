fa37d28b0e0c7ca4087d7a924346a37f1ad789dc
Adil Lari <adillari@pm.me>
2024-09-28 17:56:31 -0500

    add callback to bump siblings before saving new record

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,11 @@ class Task < ApplicationRecord
   belongs_to :lane
   has_one :board, through: :lane
   default_scope { order(:position) }
+  before_save :bump_siblings, if: :new_record?
+
+private
+
+  def bump_siblings
+    lane.tasks.update_all("position = position + 1")
+  end
 end
