bb6a4efcb753c797b145c9c2950362e7a4d26285
Adil Lari <adillari@pm.me>
2024-09-29 22:41:11 -0500

    add before_destroy commit and refactor

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,27 +1,23 @@
 class Task < ApplicationRecord
   belongs_to :lane
   has_one :board, through: :lane
-  default_scope { order(:position) }
   before_save :adjust_positions
+  before_destroy :slash_elders
+  default_scope { order(:position) }
 
 private
 
-  def bump_elders
-    Current.user.lanes.find(lane_id).tasks
-      .where("position >= ?", position)
-      .update_all("position = position + 1")
-  end
+  def adjust_positions
+    return bump_elders if new_record? # new task created
+    return bump_elders && slash_previous_elders if lane_id_changed? # task moved to different lane
+    return unless position_changed? # nothing happened (picked and dropped in same place)
 
-  def slash_previous_elders
-    Current.user.lanes.find(lane_id_was).tasks
-      .where("position > ?", position_was)
-      .update_all("position = position - 1")
-  end
-
-  def slash_hopped
-    Current.user.lanes.find(lane_id).tasks
-      .where("position > ? and position <= ?", position_was, position)
-      .update_all("position = position - 1")
+    case # task moved inside same lane
+    when position > position_was
+      slash_hopped
+    when position < position_was
+      bump_hopped
+    end
   end
 
   def bump_hopped
@@ -30,23 +26,27 @@ private
       .update_all("position = position + 1")
   end
 
-  def adjust_positions
-    return unless lane_changed? || position_changed?
-    return bump_elders if new_record?
-
-    if lane_id_changed?
-      slash_previous_elders
-      bump_elders
-    else
-      if position_grew?
-        slash_hopped
-      else
-        bump_hopped
-      end
-    end
+  def slash_hopped
+    Current.user.lanes.find(lane_id).tasks
+      .where("position > ? and position <= ?", position_was, position)
+      .update_all("position = position - 1")
   end
 
-  def position_grew?
-    position > position_was
+  def bump_elders
+    Current.user.lanes.find(lane_id).tasks
+      .where("position >= ?", position)
+      .update_all("position = position + 1")
+  end
+
+  def slash_elders
+    Current.user.lanes.find(lane_id).tasks
+      .where("position > ?", position)
+      .update_all("position = position - 1")
+  end
+
+  def slash_previous_elders
+    Current.user.lanes.find(lane_id_was).tasks
+      .where("position > ?", position_was)
+      .update_all("position = position - 1")
   end
 end
