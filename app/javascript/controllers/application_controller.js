1ecdb738b53223c35a4cd50c02890800bcb5ebd0
Adil Lari <adillari@pm.me>
2024-11-04 23:01:12 -0600

    refresh board after coming back to show view

--- app/javascript/controllers/application_controller.js
+++ app/javascript/controllers/application_controller.js
@@ -1,53 +1,75 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["modal", "indicator"]
+  static targets = ["modal", "indicator"];
 
   connect() {
-    document.addEventListener('turbo:submit-start', this.removeModal.bind(this))
-    document.addEventListener('turbo:before-fetch-request', this.#showSpinner.bind(this))
-    document.addEventListener('turbo:before-fetch-response', this.#hideSpinner.bind(this))
-    window.addEventListener('focus', () => {
-      console.log('Window is focused');
-    })
-    window.addEventListener('blur', () => {
-      console.log('Window is blurred');
-    })
-    document.addEventListener('visibilitychange', () => {
-      if (document.visibilityState === 'visible') {
-        console.log('Page is visible');
-      } else {
-        console.log('Page is hidden');
-      }
-    })
+    addEventListener("focus", this.#focusWindow);
+    addEventListener("blur", this.#blurWindow);
+    document.addEventListener(
+      "turbo:submit-start",
+      this.removeModal.bind(this),
+    );
+    document.addEventListener(
+      "turbo:before-fetch-request",
+      this.#showSpinner.bind(this),
+    );
+    document.addEventListener(
+      "turbo:before-fetch-response",
+      this.#hideSpinner.bind(this),
+    );
   }
 
   disconnect() {
-    document.removeEventListener('turbo:submit-start', this.removeModal.bind(this))
-    document.removeEventListener('turbo:before-fetch-request', this.#showSpinner.bind(this))
-    document.removeEventListener('turbo:before-fetch-response', this.#hideSpinner.bind(this))
+    removeEventListener("focus", this.#focusWindow);
+    removeEventListener("blur", this.#blurWindow);
+    document.removeEventListener(
+      "turbo:submit-start",
+      this.removeModal.bind(this),
+    );
+    document.removeEventListener(
+      "turbo:before-fetch-request",
+      this.#showSpinner.bind(this),
+    );
+    document.removeEventListener(
+      "turbo:before-fetch-response",
+      this.#hideSpinner.bind(this),
+    );
   }
 
   removeModal() {
-    this.hasModalTarget && this.modalTarget.remove()
+    this.hasModalTarget && this.modalTarget.remove();
   }
 
   stopPropagation(event) {
-    event.stopPropagation()
+    event.stopPropagation();
   }
 
   // private
 
+  #blurWindow() {
+    if (document.getElementById("board")) {
+      document.body.classList.add("opacity-50");
+    }
+  }
+
+  #focusWindow() {
+    if (document.getElementById("board")) {
+      Turbo.visit(location.pathname, { frame: "board" });
+      document.body.classList.remove("opacity-50");
+    }
+  }
+
   #showSpinner(event) {
-    if (this.#isPrefetchRequest(event)) return
-    this.hasIndicatorTarget && (this.indicatorTarget.hidden = false)
+    if (this.#isPrefetchRequest(event)) return;
+    this.hasIndicatorTarget && (this.indicatorTarget.hidden = false);
   }
 
   #hideSpinner() {
-    this.hasIndicatorTarget && (this.indicatorTarget.hidden = true)
+    this.hasIndicatorTarget && (this.indicatorTarget.hidden = true);
   }
 
   #isPrefetchRequest(event) {
-    return (event.detail.fetchOptions.headers["X-Sec-Purpose"] === "prefetch")
+    return (event.detail.fetchOptions.headers["X-Sec-Purpose"] === "prefetch");
   }
 }
