b15221576ba48581d0b9f9a5fa359b55a9ec75cd
Adil Lari <adillari@pm.me>
2024-11-17 21:48:35 -0600

    save emails locally in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,11 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  # Send mail to tmp/emails directory.
+  config.action_mailer.delivery_method = :file
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.file_settings = { location: Rails.root.join("tmp", "emails") }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
