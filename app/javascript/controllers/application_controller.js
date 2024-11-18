d92bc5d9a10261c997a745ae7d66930af7bedf12
Adil Lari <adillari@pm.me>
2024-11-17 23:23:17 -0600

    add todo for stuff that should not really be in application controller

--- app/javascript/controllers/application_controller.js
+++ app/javascript/controllers/application_controller.js
@@ -5,30 +5,33 @@ export default class extends Controller {
 
   connect() {
     // For stupid bug in Firefox on Linux/X11 where dragging blurs the window.
-    this.firefoxOnLinux =
+    this.firefoxOnLinux = // TODO: move this stuff to board js controller
       /Firefox/.test(navigator.userAgent) &&
       /Linux/.test(navigator.userAgent) &&
       !/Mobile/.test(navigator.userAgent);
 
-    this.onDragStart = () => (this.dragging = true);
-    this.bindedFocusWindow = this.#focusWindow.bind(this);
-    this.bindedBlurWindow = this.#blurWindow.bind(this);
+    this.onDragStart = () => (this.dragging = true); // TODO: move this stuff to board js controller
+    this.bindedFocusWindow = this.#focusWindow.bind(this); // TODO: move this stuff to board js controller
+    this.bindedBlurWindow = this.#blurWindow.bind(this); // TODO: move this stuff to board js controller
+
     this.bindedRemoveModal = this.removeModal.bind(this);
     this.bindedShowSpinner = this.#showSpinner.bind(this);
     this.bindedHideSpinner = this.#hideSpinner.bind(this);
 
-    document.addEventListener("dragstart", this.onDragStart);
-    addEventListener("focus", this.bindedFocusWindow);
-    addEventListener("blur", this.bindedBlurWindow);
+    document.addEventListener("dragstart", this.onDragStart); // TODO: move this stuff to board js controller
+    addEventListener("focus", this.bindedFocusWindow); // TODO: move this stuff to board js controller
+    addEventListener("blur", this.bindedBlurWindow); // TODO: move this stuff to board js controller
+  
     document.addEventListener("turbo:submit-start", this.bindedRemoveModal);
     document.addEventListener("turbo:before-fetch-request", this.bindedShowSpinner);
     document.addEventListener("turbo:before-fetch-response", this.bindedHideSpinner);
   }
 
   disconnect() {
-    document.removeEventListener("dragstart", this.onDragStart);
-    removeEventListener("focus", this.bindedFocusWindow);
-    removeEventListener("blur", this.bindedBlurWindow);
+    document.removeEventListener("dragstart", this.onDragStart); // TODO: move this stuff to board js controller
+    removeEventListener("focus", this.bindedFocusWindow); // TODO: move this stuff to board js controller
+    removeEventListener("blur", this.bindedBlurWindow); // TODO: move this stuff to board js controller
+
     document.removeEventListener("turbo:submit-start", this.bindedRemoveModal);
     document.removeEventListener("turbo:before-fetch-request", this.bindedShowSpinner);
     document.removeEventListener("turbo:before-fetch-response", this.bindedHideSpinner);
@@ -44,7 +47,7 @@ export default class extends Controller {
 
   // private
 
-  #blurWindow() {
+  #blurWindow() { // TODO: move this stuff to board js controller
     // For stupid bug in Firefox on Linux/X11 where dragging blurs the window.
     if (this.firefoxOnLinux && this.dragging) return;
 
@@ -53,7 +56,7 @@ export default class extends Controller {
     }
   }
 
-  #focusWindow() {
+  #focusWindow() { // TODO: move this stuff to board js controller
     // For stupid bug in Firefox on Linux/X11 where dragging blurs the window.
     if (this.firefoxOnLinux && this.dragging) return (this.dragging = false);
 
