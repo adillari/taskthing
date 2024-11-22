a6a48cf7c9b61a634681b9e255d30be0cc26cf20
Adil Lari <adillari@pm.me>
2024-11-22 09:58:34 -0600

    configure sidekiq as AJ queue backend

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Taskthing
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # Use a real queuing backend for Active Job.
+    config.active_job.queue_adapter = :sidekiq
   end
 end
