77e0b72e8e127b9e1e0ea91add9566a7136f381c
Adil Lari <adillari@pm.me>
2024-11-29 20:59:46 -0600

    Invites - mailers and #show

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,8 +1,23 @@
 class InvitesController < ApplicationController
   def new
-    @invite = Invite.new
+    @invite = Invite.new(board_id: params[:board_id])
   end
 
   def create
+    raise if invite_params[:email_address] == Current.user.email_address # can't invite yourself duh
+
+    invite = Current.user.invites.create(invite_params)
+
+    BoardsMailer.invite(invite).deliver_later
+  end
+
+  def show
+    @invite = Invite.find(params[:id])
+  end
+
+  private
+
+  def invite_params
+    params.require(:invite).permit(:email_address, :board_id)
   end
 end
