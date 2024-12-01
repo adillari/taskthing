8911e2798792cbc7fce32912d8dda6d38620047f
Adil Lari <adillari@pm.me>
2024-12-01 14:23:53 -0600

    BoardPreviewComponent - move ruby code from template to component

--- app/components/board_preview_component.rb
+++ app/components/board_preview_component.rb
@@ -5,4 +5,30 @@ class BoardPreviewComponent < ApplicationComponent
     super
     @board = board
   end
+
+  def link_to_board
+    link_to(board_path(id: @board.id), class: "p-4 grow") do
+      tag.span(class: "overflow-hidden") do
+        @board.title
+      end
+    end
+  end
+
+  def edit_button
+    link_to(
+      "edit",
+      edit_board_path(@board),
+      class: "material-symbols-outlined ml-auto text-yellow-500 m-1",
+      data: { turbo_frame: :modal },
+    )
+  end
+
+  def delete_button
+    # link_to(
+    #   "delete",
+    #   delete_confirmation_board_path(@board),
+    #   class: "material-symbols-outlined ml-auto text-red-500",
+    #   data: { turbo_frame: :modal },
+    # )
+  end
 end
