7b117397a59db05d82800a57eea76b05017c0374
Adil Lari <adillari@pm.me>
2024-12-02 08:38:10 -0600

    fix mispelling of collaborate

--- app/mailers/boards_mailer.rb
+++ app/mailers/boards_mailer.rb
@@ -1,6 +1,6 @@
 class BoardsMailer < ApplicationMailer
   def invite(invite)
     @invite = invite
-    mail(subject: "Invite to Collaberate on \"#{@invite.board.title}\"", to: @invite.email_address)
+    mail(subject: "Invite to Collaborate on \"#{@invite.board.title}\"", to: @invite.email_address)
   end
 end
