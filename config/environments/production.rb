329d5cc00cae051eeadee8714e452b947e78e46a
Adil Lari <adillari@pm.me>
2024-12-21 18:48:45 -0600

    use host.docker.internal

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
 
   # Use a different cache store in production.
-  config.cache_store = :redis_cache_store, { url: "redis://172.17.0.1:6379/0" }
+  config.cache_store = :redis_cache_store, { url: "redis://host.docker.internal:6379/0" }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
