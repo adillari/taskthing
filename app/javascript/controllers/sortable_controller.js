6874923b1be03d34b9750e968282756520e1bcbc
Adil Lari <adillari@pm.me>
2025-01-05 16:48:14 -0600

    show board members and list lanes.

---
+++ app/javascript/controllers/sortable_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus";
+import Sortable from "sortablejs";
+
+export default class extends Controller {
+  connect() {
+    this.sortable = Sortable.create(this.element, {
+      ghostClass: "opacity-0",
+      chosenClass: "rotate-1",
+      // dragClass: "rotate-2", // doesn't seem to work
+      draggable: "li",
+      animation: 225,
+      delay: 225,
+	    delayOnTouchOnly: true,
+      touchStartThreshold: 5,
+    });
+  }
+
+  disconnect() {
+    this.sortable.destroy();
+  }
+}
+
