99dca4c3bac2baa07d6a44c38c4b1cf4593560cb
Adil Lari <adillari@pm.me>
2025-01-11 01:36:28 -0600

    fix board broadcast bug when deleting

--- app/models/concerns/broadcasts/board.rb
+++ app/models/concerns/broadcasts/board.rb
@@ -9,10 +9,13 @@ module Broadcasts
     private
 
     def update_board
+      return unless board
+      return unless (preloaded_board = ::Board.includes(lanes: [:tasks]).find_by(id: board.id))
+
       broadcast_replace_to(
         board,
         target: :board,
-        renderable: BoardComponent.new(board: ::Board.includes(lanes: [:tasks]).find(board.id)),
+        renderable: BoardComponent.new(board: preloaded_board),
       )
     end
   end
