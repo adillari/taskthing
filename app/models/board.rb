a9bde652c5eb7501d752be92e180d27e2c4b2b7d
Adil Lari <adillari@pm.me>
2025-01-11 01:35:50 -0600

    add invites and tasks relations to Board

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,7 +1,9 @@
 class Board < ApplicationRecord
   has_many :board_users, dependent: :destroy
   has_many :users, through: :board_users
+  has_many :invites, dependent: :destroy
   has_many :lanes, dependent: :destroy
+  has_many :tasks, through: :lanes
   after_create_commit :create_default_lanes
 
   private
