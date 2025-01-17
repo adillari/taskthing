2e9601c1358fec298b372861a4b91cba1b276d73
Adil Lari <adillari@pm.me>
2025-01-16 21:38:53 -0600

    use button_to over link_to(because holding link on mobile opens  dialog)

--- app/components/board_preview_component.rb
+++ app/components/board_preview_component.rb
@@ -7,7 +7,7 @@ class BoardPreviewComponent < ApplicationComponent
   end
 
   def link_to_board
-    link_to(board_path(id: @board.id), class: "p-4 grow") do
+    button_to(board_path(id: @board.id), method: :get, class: "p-4 w-full", form_class: "w-full") do
       tag.span(class: "overflow-hidden") do
         @board.title
       end
