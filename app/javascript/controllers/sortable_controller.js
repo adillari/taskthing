365641832beaa3711b52b7e4f6af8ffcdb370873
Adil Lari <adillari@pm.me>
2025-01-18 17:13:38 -0600

    Remove BoardPreviewComponent

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -5,8 +5,10 @@ export default class extends Controller {
   connect() {
     this.sortable = Sortable.create(this.element, {
       ghostClass: "opacity-0",
+      // chosenClass: "rotate-1", make this configurable
       // dragClass: "rotate-2", // doesn't seem to work
       draggable: "li",
+      // handle: ".handle", // make configurable
       animation: 225,
       delay: 225,
       delayOnTouchOnly: true,
