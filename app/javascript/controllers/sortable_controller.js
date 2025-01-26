b0113fe2763247f2143370ee3eee04cb8ad991c1
Adil Lari <adillari@pm.me>
2025-01-26 16:11:59 -0600

    update stimulus_controller to be more flexible

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -2,20 +2,27 @@ import { Controller } from "@hotwired/stimulus";
 import Sortable from "sortablejs";
 
 export default class extends Controller {
-  static values = { submit: { type: Boolean, default: false } };
+  static values = {
+    onEnd: { type: String, default: null },
+    draggable: { type: String, default: "li" },
+    delayOnMobile: { type: Boolean, default: true },
+    handle: { type: String, default: null },
+  };
 
   connect() {
-    this.sortable = Sortable.create(this.element, {
-      ghostClass: "opacity-0",
-      // chosenClass: "rotate-1", make this configurable
-      // dragClass: "rotate-2", // doesn't seem to work
-      draggable: "li",
-      // handle: ".handle", // make configurable
+    let config = {
+      ghostClass: "!opacity-0",
+      draggable: this.draggableValue,
       animation: 225,
-      delay: 225,
-      delayOnTouchOnly: true,
       touchStartThreshold: 5,
-    });
+    };
+    if (this.handleValue) config.handle = this.handleValue;
+    if (this.onEndValue) config.onEnd = this.onEndValue;
+    if (this.delayOnMobileValue) {
+      config.delay = 225;
+      config.delayOnTouchOnly = true;
+    }
+    this.sortable = Sortable.create(this.element, config);
   }
 
   disconnect() {
