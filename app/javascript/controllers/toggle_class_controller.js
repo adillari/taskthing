6d302e8a9db1cfe5b8cd8268388590ef095f751a
Adil Lari <adillari@pm.me>
2025-01-28 23:45:32 -0600

    delete lane

---
+++ app/javascript/controllers/toggle_class_controller.js
@@ -0,0 +1,9 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static values = { class: String }
+
+  toggle() {
+    this.element.classList.toggle(this.classValue);
+  }
+}
