5b3d7bad5054294f47ca4ab7357eaa7bad634c94
Adil Lari <adillari@pm.me>
2025-04-13 03:02:01 -0500

    update

--- app/javascript/controllers/custom_stream_actions.js
+++ app/javascript/controllers/custom_stream_actions.js
@@ -17,4 +17,18 @@ StreamActions.place_new_task_unless_up_to_date = function() {
   }
 }
 
+StreamActions.update_task_unless_up_to_date = function() {
+  const lane = this.targetElements[0]
+  const ul = lane.querySelector("ul")
+  const fragment = this.templateElement.content
+  const version = fragment.firstElementChild.dataset.version
+  const taskId = fragment.firstElementChild.id
+  const position = Number(fragment.firstElementChild.querySelector("input[name='task[position]']").value)
+
+  if (document.getElementById("version")?.innerText !== version) {
+    document.getElementById(taskId).remove()
+    ul.insertBefore(fragment.firstElementChild, ul.children[position].nextSibling)
+  }
+}
+
 export { StreamActions }
