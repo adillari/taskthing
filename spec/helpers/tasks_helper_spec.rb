fdfee6f000e7d27d14cd51f10aba9ca016cb6f4f
Adil Lari <adillari@pm.me>
2025-04-13 23:57:00 -0500

    remove specs

--- spec/helpers/tasks_helper_spec.rb
+++ spec/helpers/tasks_helper_spec.rb
@@ -1,20 +1,7 @@
 require "rails_helper"
 
 RSpec.describe(TasksHelper, type: :helper) do
-  # It's very annoying to use a double here because of the many methods the form_with helper calls on the model
   let(:task) { Task.new(id: 5, position: 8, lane_id: 2, title: "Task Title", description: "Task Description") }
-
-  describe "#task_form" do
-    it "renders a form with hidden fields for lane_id and position" do
-      result = helper.task_form(task)
-
-      expect(result).to(have_css('form[method="post"][action="/tasks"][accept-charset="UTF-8"]'))
-      within("form") do
-        expect(result).to(have_css('input[type="hidden"][name="task[lane_id]"][value="2"]'))
-        expect(result).to(have_css('input[type="hidden"][name="task[position]"][value="8"]'))
-      end
-    end
-  end
 
   describe "#task_edit_button" do
     it "returns a link to edit the task" do
@@ -38,32 +25,10 @@ RSpec.describe(TasksHelper, type: :helper) do
     end
   end
 
-  describe "#task_description" do
-    context "when task has a description" do
-      it "returns a paragraph with the task description" do
-        result = helper.task_description(task)
-
-        expect(result).to(have_css(
-          "p.h-full.border-t.border-zinc-700.mt-1\\.5.pt-1\\.5.grow",
-          text: "Task Description",
-        ))
-      end
-    end
-
-    context "when task has no description" do
-      it "returns nil" do
-        allow(task).to(receive(:description).and_return(nil))
-        result = helper.task_description(task)
-
-        expect(result).to(be_nil)
-      end
-    end
-  end
-
   describe "#task_classes" do
     it "returns the expected CSS classes" do
       expect(helper.task_classes)
-        .to(eq("bg-zinc-950 border-2 border-zinc-700 border-l-4 border-l-violet-700 rounded shadow-lg"))
+        .to(eq("bg-zinc-950 border-2 border-zinc-700 border-l-4 border-l-violet-700 rounded shadow-lg task flex cursor-grab select-none"))
     end
   end
 end
