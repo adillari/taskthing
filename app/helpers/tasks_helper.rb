7b1351b4fdf707a7a25d68648110815838fcd9e8
Adil Lari <adillari@pm.me>
2025-04-13 23:52:41 -0500

    remove some helpers

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,14 +1,4 @@
 module TasksHelper
-  # Form for existing Task that gets submitted when its picked up and dropped anywhere on the board
-  def task_form(task)
-    form_with(model: task) do |form|
-      safe_join([
-        form.hidden_field(:lane_id, id: nil), # id attributes are nil because rails created duplicate dom id's otherwise
-        form.hidden_field(:position, id: nil), # and that botthers me because its not considered valid html
-      ])
-    end
-  end
-
   def task_edit_button(task)
     link_to(
       "edit",
@@ -27,17 +17,17 @@ module TasksHelper
     )
   end
 
-  def task_description(task)
-    if task.description.present?
-      tag.p(task.description, class: "h-full border-t border-zinc-700 mt-1.5 pt-1.5 grow")
-    end
-  end
-
   def new_task_classes
-    "#{task_classes} !mt-0 !mb-2 p-2 shrink-0"
+    "#{shared_task_classes} p-2"
   end
 
   def task_classes
+    "#{shared_task_classes} task flex cursor-grab select-none"
+  end
+
+  private
+
+  def shared_task_classes
     "bg-zinc-950 border-2 border-zinc-700 border-l-4 border-l-violet-700 rounded shadow-lg"
   end
 end
